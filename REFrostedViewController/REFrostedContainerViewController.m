0b49c56763cae584acc84800a4a4adb4825b044f
Kim Le <kle@jawbone.com>
2016-01-29 17:36:57 -0800

    UPFOUR-8771 Cleaning up logs in REFrosted to debug side panel issues.

--- REFrostedViewController/REFrostedContainerViewController.m
+++ REFrostedViewController/REFrostedContainerViewController.m
@@ -90,32 +90,9 @@
     [self.view addGestureRecognizer:self.frostedViewController.panGestureRecognizer];
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-
-	NSLogUITesting(@"show menu REFrostedContainerViewController viewWillAppear 1/1");
-	//[self showFrostedViewController];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-	[super viewDidAppear:animated];
-
-	NSLogUITesting(@"show menu REFrostedContainerViewController viewDidAppear 1/1");
-
-	// Need to call again since viewWillAppear does not always get called. (FoundByAutomation)
-	//[self showFrostedViewController];
-}
-
 - (void)showFrostedViewController
 {
-	NSLogUITesting(@"show menu REFrostedContainerViewController showFrostedViewController start");
-
 	if(!self.frostedViewController.visible) {
-
-		NSLogUITesting(@"show menu REFrostedContainerViewController showFrostedViewController do something");
-
 		self.backgroundImageView.image = self.screenshotImage;
 		self.backgroundImageView.frame = self.view.bounds;
 		self.frostedViewController.menuViewController.view.frame = self.containerView.bounds;
@@ -137,13 +114,8 @@
 		}
 
 		if (self.animateApperance) {
-			NSLogUITesting(@"show menu REFrostedContainerViewController showFrostedViewController");
 			[self show];
-		} else {
-			NSLogUITesting(@"show menu animateApperance is false !!!!!!!!!!!!!!!!!!!!!!!!!");
 		}
-	} else {
-		NSLogUITesting(@"show menu We think menu is already visiable!!!!!!!!!!!!!!!!!!!!!!!!!");
 	}
 }
 
